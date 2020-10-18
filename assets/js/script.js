030eb4d0c655da200a64dc27e8ab6beae75ee969
DryBloodRaven <amproctor@gmail.com>
2020-10-18 16:28:40 -0700

    questions are generating, but timer not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 // Variables being assinged to the whole page
-var startBtn = document.getElementById("btn-start");
-var countdown = document.getElementById("countdown");
-var quizTitle = document.querySelector("#quiz-title");
-var instructions = document.querySelector("#instructions");
+var startBtn = document.querySelector("#btn-start");
+var countdown = document.querySelector("#countdown");
+var startContent = document.querySelector("#start-container");
+
+var questionContainer = document.querySelector("#question-container");
+var questionTitle = document.querySelector("#question-title");
+var questionChoices = document.querySelector("#choices");
+var answerDiv = document.querySelector("#answer");
+
+var endResult = document.querySelector("#end-container");
+
+var qIndex = 0;
 
 // Start Quiz function where timer is stored
 function startQuiz() {
@@ -19,18 +27,77 @@ function startQuiz() {
             allDone(countdownTimer);
         }
     }, 1000);
+
+    startContent.style.display = "none";
+    questionContainer.style.display = "flex";
+    endResult.style.display = "none";
+
+    getQuestion();
 };
 
-if (questions.choices === questions.answer) {
-    
-}
 
+// Get Questions
+var getQuestion = function () {
+    var firstQuestion = questions[qIndex];
+    questionTitle.textContent = firstQuestion.question;
+    questionChoices.innerHTML = "";
 
+    firstQuestion.choices.forEach(function(choice, i) {
+        var selectChoice = document.createElement("button");
+        selectChoice.setAttribute("class", "btn-choice");
+        selectChoice.setAttribute("value", choice);
+        selectChoice.textContent = i + 1 + ". " + choice;
 
-startBtn.addEventListener("click", function(){
-    quizTitle.classList.toggle("hidden");
-    instructions.classList.toggle("hidden");
-    startBtn.classList.toggle("hidden");
+        selectChoice.addEventListener("click", nextQuestion);
+        questionChoices.appendChild(selectChoice);
+    });
+};
 
-    startQuiz();
-});
+var nextQuestion = function() {
+    var answer = document.createElement("h3");
+    answer.setAttribute("class", "answer");
+    answerDiv.appendChild(answer);
+
+    if (this.value === questions[qIndex].answer) {
+        answer.textContent = "Correct!"
+        setTimeout(function() {
+            answer.remove();
+        }, 1500);
+    } else {
+        answer.textContent = "Wrong!";
+        countdownTimer = countdownTimer - 10;
+        if (countdownTimer <= 10){
+            countdownTimer = 0;
+            clearInterval(timer);
+        }
+        setTimeout(function() {
+            answer.remove();
+        }, 1500);
+    }
+
+    qIndex++;
+
+    if (qIndex === questions.length) {
+        stopQuiz();
+    } else {
+        getQuestion();
+    }
+};
+
+var stopQuiz = function() {
+    countdown.textContent = countdownTimer;
+    clearInterval(timer);
+
+    questionContainer.style.display = "none";
+    endResult.style.display = "flex";
+
+    endResult.textContent = "Your new highscore is: " + countdownTimer + "!";
+};
+
+// Move from question to question
+
+// End Game
+
+// High Score List
+
+startBtn.addEventListener("click", startQuiz);
